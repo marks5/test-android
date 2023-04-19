ba583c7ac2086c3b6e571e0a1f264b3428b8ddf1
gmrksao <119072764+gmrksao@users.noreply.github.com>
2023-04-19 16:08:12 -0300

    ui

---
+++ app/src/main/java/com/example/infinitscroll/presentation/SearchRepositoriesActivity.kt
@@ -0,0 +1,84 @@
+package com.example.infinitscroll.presentation
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.*
+import androidx.paging.LoadState
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.example.infinitscroll.Injection
+import com.example.infinitscroll.databinding.ActivitySearchRepositoriesBinding
+import com.example.infinitscroll.domain.Repo
+import com.example.infinitscroll.domain.RepoSearchResult
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class SearchRepositoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySearchRepositoriesBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val viewModel = ViewModelProvider(this, Injection.provideViewModelFactory(owner = this))
+            .get(SearchRepositoriesViewModel::class.java)
+
+        val decoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        binding.list.addItemDecoration(decoration)
+
+        binding.bindState(
+            uiState = viewModel.state,
+            pagingData = viewModel.pagingDataFlow,
+            uiActions = viewModel.accept
+        )
+    }
+
+    private fun ActivitySearchRepositoriesBinding.bindState(
+        uiState: StateFlow<UiState>,
+        pagingData: Flow<PagingData<Repo>>,
+        uiActions: (UiAction) -> Unit
+    ) {
+        val repoAdapter = ReposAdapter()
+        list.adapter = repoAdapter
+
+        bindList(
+            repoAdapter = repoAdapter,
+            uiState = uiState,
+            pagingData = pagingData,
+            onScrollChanged = uiActions,
+        )
+    }
+
+    private fun ActivitySearchRepositoriesBinding.bindList(
+        repoAdapter: ReposAdapter,
+        uiState: StateFlow<UiState>,
+        pagingData: Flow<PagingData<Repo>>,
+        onScrollChanged: (UiAction.Scroll) -> Unit
+    ) {
+        list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy != 0) onScrollChanged(UiAction.Scroll(currentQuery = uiState.value.query))
+            }
+        })
+
+        lifecycleScope.launch {
+            pagingData.collectLatest { repoAdapter.submitData(it) }
+        }
+
+        lifecycleScope.launch {
+            repoAdapter.loadStateFlow.collect { loadState ->
+                val isListEmpty = loadState.refresh is LoadState.NotLoading && repoAdapter.itemCount == 0
+                pb.isVisible = loadState.source.refresh is LoadState.Loading
+                list.isVisible = !isListEmpty
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/infinitscroll/presentation/UiAction.kt
@@ -0,0 +1,14 @@
+package com.example.infinitscroll.presentation
+
+private const val DEFAULT_QUERY = "language:kotlin"
+
+sealed class UiAction {
+    data class Search(val query: String) : UiAction()
+    data class Scroll(val currentQuery: String) : UiAction()
+}
+
+data class UiState(
+    val query: String = DEFAULT_QUERY,
+    val lastQueryScrolled: String = DEFAULT_QUERY,
+    val hasNotScrolledForCurrentSearch: Boolean = false
+)
