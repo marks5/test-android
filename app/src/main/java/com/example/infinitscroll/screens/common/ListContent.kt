0a92b3eb05db884a4d04109ba7fd5dc8b0359cd9
gmrksao <119072764+gmrksao@users.noreply.github.com>
2023-04-26 13:48:42 -0300

    better with refresh state

--- app/src/main/java/com/example/infinitscroll/screens/common/ListContent.kt
+++ app/src/main/java/com/example/infinitscroll/screens/common/ListContent.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,15 +16,19 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
+import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.items
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.example.infinitscroll.R
 import com.example.infinitscroll.domain.Repo
+import kotlinx.coroutines.flow.Flow
 
 @ExperimentalCoilApi
 @Composable
@@ -40,6 +46,81 @@ fun ListContent(items: LazyPagingItems<Repo>) {
             }
         ) { each ->
             each?.let { RepoItem(repo = it) }
+        }
+
+        val loadState = items.loadState.mediator
+        item {
+            if (loadState?.refresh == LoadState.Loading) {
+                Column(modifier = Modifier.fillParentMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center) {
+                    Text(
+                        modifier = Modifier.padding(8.dp),
+                        text = "Refresh Loading"
+                    )
+
+                    CircularProgressIndicator(color = MaterialTheme.colors.primary)
+                }
+            }
+
+            if (loadState?.append == LoadState.Loading) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    CircularProgressIndicator(color = MaterialTheme.colors.primary)
+                }
+            }
+
+            if (loadState?.refresh is LoadState.Error || loadState?.append is LoadState.Error) {
+                val isPaginatingError =
+                    (loadState.append is LoadState.Error) || items.itemCount > 1
+                val error = if (loadState.append is LoadState.Error)
+                    (loadState.append as LoadState.Error).error
+                else
+                    (loadState.refresh as LoadState.Error).error
+
+                val modifier = if (isPaginatingError) {
+                    Modifier.padding(8.dp)
+                } else {
+                    Modifier.fillParentMaxSize()
+                }
+                Column(
+                    modifier = modifier,
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    if (!isPaginatingError) {
+                        Icon(
+                            modifier = Modifier
+                                .size(64.dp),
+                            imageVector = Icons.Rounded.Warning, contentDescription = null
+                        )
+                    }
+
+                    Text(
+                        modifier = Modifier
+                            .padding(8.dp),
+                        text = error.message ?: error.toString(),
+                        textAlign = TextAlign.Center,
+                    )
+
+                    Button(
+                        onClick = {
+                            items.refresh()
+                        },
+                        content = {
+                            Text(text = "Refresh")
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = MaterialTheme.colors.primary,
+                            contentColor = Color.White,
+                        )
+                    )
+                }
+            }
         }
     }
 }
